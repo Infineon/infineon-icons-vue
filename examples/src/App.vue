79ce257193f11ba60db53c9d04d5d70e50a739c5
werther <kai.werther@infineon.com>
2022-09-30 10:30:05 +0200

    added examples + deployment to gibhub pages

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="d-flex flex-row h-100">
-    <infineon-icon-vue
-      icon="cCheck12"
-    />
-    <infineon-icon-vue
-      icon="c-check-12"
-    />
-    <p style="color:blue">
-      <infineon-icon-vue
-        icon="cart12"
-      />
-    </p>
-    <infineon-icon-vue
-      icon="cart-12"
-    />
+    <div>
+      Filter: <input v-model="searchText">
+    </div>
+    <div
+      v-for="icon in searchedIcons"
+      :key="icon.name"
+      style="display:flex;margin-top:0.5em"
+    >
+      <div style="width:3em; display: flex; align-items: center;">
+        <infineon-icon-vue
+          :icon="icon.name"
+        />
+      </div>
+      <div>
+        <code>name: <b>{{ icon.name }}</b> // <b>{{ icon.alternativeName }}</b><br>
+          <b>&lt;infineon-icon-vue icon="{{ icon.name }}"&gt;</b></code>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import library from '@infineon/infineon-icons/library';
+import { ref, computed } from 'vue';
 import { InfineonIconVue } from '../../lib';
+
+const searchText = ref('');
+
+const searchedIcons = computed(() => Object.values(library.getIcons())
+  .filter((i) => i.name.includes(searchText.value)
+  || i.alternativeName.includes(searchText.value)));
 
 </script>
--- examples/src/icons.js
+++ examples/src/icons.js
@@ -1,4 +1,12 @@
-import { cCheck12, cart12 } from '@infineon/infineon-icons';
+import * as icons from '@infineon/infineon-icons';
 import library from '@infineon/infineon-icons/library';
 
-library.add(cCheck12, cart12);
+library.add(...Object.values(icons));
+
+// normally you would never import all icons because it just increases your bundle size
+// and leads to slower page loading
+
+// you want to load each icon seperately to enable tree shaking
+
+// import { cCheck12, cart12 } from '@infineon/infineon-icons';
+// library.add(cCheck12, cart12);
